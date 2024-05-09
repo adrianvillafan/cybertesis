beff60d1665e40c9fd5eaf5c9b1b70c3ddd3dac2
CRISTINAMARTINEZZ <luzcristina.martinez@unmsm.edu.pe>
2024-05-09 17:26:06 -0500

    tdtrjtyft

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+// Cargar las variables de entorno desde el archivo .env
+
 
 const pool = mysql.createPool({
-  host: 'sql.freedb.tech',
-  user: 'freedb_marcelasdasd',
-  password: 'tCADemhZRPF39d!',
-  database: 'freedb_Cybertesis',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
