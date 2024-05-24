aa918a82c1695c67243c11cefe704c034157db49
adrianvillafan <adrian.villafan@unmsm.edu.pe>
2024-05-24 15:52:23 -0500

    hooia
    araert

--- src/components/dashboard/views/escuela_upg/modals/TesisModal.jsx
+++ src/components/dashboard/views/escuela_upg/modals/TesisModal.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import { ColumnLayout ,Modal, FormField, Input, FileUpload, Button, Box, Select, SpaceBetween, Container, Spinner, Header } from '@cloudscape-design/components';
+import { Modal, FormField, Input, FileUpload, Button, Box, Select, SpaceBetween, Container, Spinner, Header, ColumnLayout } from '@cloudscape-design/components';
 import { fetchDatosByDni } from '../../../../../../api';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 
 const TesisModal = ({ onClose, alumnoData }) => {
   const [file, setFile] = useState(null);
   const [fileUrl, setFileUrl] = useState('');
   const [showForm, setShowForm] = useState(false);
-  const [loadingDni, setLoadingDni] = useState(false);
+  const [loadingDni, setLoadingDni] = useState({ autores: {}, asesores: {} });
+  const [numPages, setNumPages] = useState(null);
   const [formData, setFormData] = useState({
     facultad: alumnoData?.facultad || '',
     escuela: alumnoData?.escuela || '',
@@ -30,10 +32,13 @@ const TesisModal = ({ onClose, alumnoData }) => {
     }]
   });
 
+  const currentYear = new Date().getFullYear();
+  const years = Array.from({ length: 6 }, (_, i) => currentYear - i);
+
   const handleFileChange = ({ detail }) => {
     const selectedFile = detail.value[0];
-    if (selectedFile.size > 5000000) {
-      alert("El tamaño del archivo excede los 5MB.");
+    if (selectedFile.size > 15000000) {
+      alert("El tamaño del archivo excede los 15MB.");
     } else {
       setFile(selectedFile);
       setShowForm(!!selectedFile);
@@ -70,9 +75,9 @@ const TesisModal = ({ onClose, alumnoData }) => {
   };
 
   const fetchAndSetDataByDni = async (dni, index, type) => {
-    setLoadingDni(true);
+    setLoadingDni(prev => ({ ...prev, [type]: { ...prev[type], [index]: true } }));
     const data = await fetchDatosByDni(dni);
-    setLoadingDni(false);
+    setLoadingDni(prev => ({ ...prev, [type]: { ...prev[type], [index]: false } }));
     if (data) {
       handleChange('nombre', data.nombre || '', index, type);
       handleChange('apellido', data.apellido || '', index, type);
@@ -117,6 +122,31 @@ const TesisModal = ({ onClose, alumnoData }) => {
     }
   };
 
+  const handleDniChange = (detail, index, type) => {
+    handleChange('dni', detail.value, index, type);
+    if (detail.value.length === 8) {
+      fetchAndSetDataByDni(detail.value, index, type);
+    } else {
+      handleChange('nombre', 'Cargando...', index, type);
+      handleChange('apellido', 'Cargando...', index, type);
+      if (type === 'autores') {
+        handleChange('telefono', 'Cargando...', index, type);
+        handleChange('email', 'Cargando...', index, type);
+      } else if (type === 'asesores') {
+        handleChange('orcid', 'Cargando...', index, type);
+      }
+    }
+  };
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+    console.log('Archivo cargado satisfactoriamente');
+  };
+
+  const onDocumentLoadError = () => {
+    console.log('Error al cargar el archivo');
+  };
+
   return (
     <Modal
       onDismiss={handleClose}
@@ -140,7 +170,7 @@ const TesisModal = ({ onClose, alumnoData }) => {
               accept="application/pdf"
               value={file ? [file] : []}
               onChange={handleFileChange}
-              constraintText="El tamaño máximo del archivo es de 5MB."
+              constraintText="El tamaño máximo del archivo es de 15MB."
               i18nStrings={{
                 dropzoneText: () => 'Arrastra los archivos aquí o haz clic para seleccionar',
                 uploadButtonText: () => 'Seleccionar archivo',
@@ -151,134 +181,162 @@ const TesisModal = ({ onClose, alumnoData }) => {
         ) : (
           <ColumnLayout columns={2} variant="default">
             <Box width="100%">
-              <iframe src={fileUrl} title="Visualizador de PDF" width="100%" height="600px" />
-            </Box>
-            <Container header={<Header variant="h3">Formulario de Tesis</Header>} fitHeight>
-              <FormField label="Facultad">
-                <Input value={formData.facultad} readOnly />
-              </FormField>
-              <FormField label="Escuela">
-                <Input value={formData.escuela} readOnly />
-              </FormField>
-              <FormField label="Título">
-                <Input value={formData.titulo} onChange={({ detail }) => setFormData(prev => ({ ...prev, titulo: detail.value }))} />
-              </FormField>
-              <FormField label="Tipo y Grado">
-                <SpaceBetween direction="horizontal" size="m">
-                  <Select
-                    selectedOption={{ label: formData.tipo, value: formData.tipo }}
-                    options={[
-                      { label: 'Tesis', value: 'Tesis' },
-                      { label: 'Trabajo de investigación', value: 'Trabajo de investigación' }
-                    ]}
-                    onChange={({ detail }) => setFormData(prev => ({ ...prev, tipo: detail.selectedOption.value }))}
-                  />
-                  <Select
-                    selectedOption={{ label: formData.grado, value: formData.grado }}
-                    options={[
-                      { label: 'Bachiller', value: 'Bachiller' },
-                      { label: 'Magister', value: 'Magister' },
-                      { label: 'Doctor', value: 'Doctor' },
-                      { label: 'Segunda Esp.', value: 'Segunda Esp.' },
-                      { label: 'Título Prof.', value: 'Título Prof.' }
-                    ]}
-                    onChange={({ detail }) => setFormData(prev => ({ ...prev, grado: detail.selectedOption.value }))}
-                  />
-                </SpaceBetween>
-              </FormField>
-              <FormField label="Año">
-                <Input value={formData.year} onChange={({ detail }) => setFormData(prev => ({ ...prev, year: detail.value }))} type="number" maxLength={4} />
-              </FormField>
-              <Container header={<Header variant="h4">Datos del Documento</Header>}>
-                {formData.autores.map((autor, index) => (
-                  <Container
-                    key={index}
-                    header={
-                      <SpaceBetween direction="horizontal" size="m">
-                        <Header variant="h5">Datos del Autor {index + 1}</Header>
-                        {index > 0 && (
-                          <Button onClick={() => removeAuthor(index)} variant="icon" iconName="close" ariaLabel="Eliminar autor" />
-                        )}
-                      </SpaceBetween>
-                    }
-                  >
-                    <SpaceBetween direction="horizontal" size="m">
-                      <FormField label="Nombres">
-                        <Input value={autor.nombre} readOnly />
-                      </FormField>
-                      <FormField label="Apellidos">
-                        <Input value={autor.apellido} readOnly />
-                      </FormField>
-                    </SpaceBetween>
-                    <SpaceBetween direction="horizontal" size="m">
-                      <FormField label="DNI">
-                        <Input value={autor.dni} readOnly={index === 0} onChange={({ detail }) => {
-                          handleChange('dni', detail.value, index);
-                          if (detail.value.length === 8) {
-                            fetchAndSetDataByDni(detail.value, index, 'autores');
-                          }
-                        }} />
-                      </FormField>
-                      <FormField label="Teléfono">
-                        <Input value={autor.telefono} readOnly={autor.telefono !== ''} onChange={({ detail }) => handleChange('telefono', detail.value, index)} />
-                      </FormField>
-                    </SpaceBetween>
-                    <FormField label="Email UNMSM">
-                      <Input value={autor.email} readOnly={autor.email !== ''} onChange={({ detail }) => handleChange('email', detail.value, index)} />
-                    </FormField>
-                  </Container>
+              <Document
+                file={fileUrl}
+                onLoadSuccess={onDocumentLoadSuccess}
+                onLoadError={onDocumentLoadError}
+              >
+                {Array.from(new Array(Math.min(numPages, 10)), (el, index) => (
+                  <Page key={`page_${index + 1}`} pageNumber={index + 1} />
                 ))}
-                <Button onClick={addAuthor} disabled={formData.autores.length >= 2}>Agregar Autor</Button>
-              </Container>
-              <Container header={<Header variant="h4">Datos del Asesor</Header>}>
-                {formData.asesores.map((asesor, index) => (
-                  <Container
-                    key={index}
-                    header={
+              </Document>
+            </Box>
+            <div style={{ height: 'calc(100vh - 200px)', overflowY: 'auto' }}>
+              <Container header={<Header variant="h3">Formulario de Tesis</Header>}>
+                <FormField label="Facultad">
+                  <Input value={formData.facultad} readOnly />
+                </FormField>
+                <FormField label="Escuela">
+                  <Input value={formData.escuela} readOnly />
+                </FormField>
+                <FormField label="Título">
+                  <Input value={formData.titulo} onChange={({ detail }) => setFormData(prev => ({ ...prev, titulo: detail.value }))} />
+                </FormField>
+                <ColumnLayout columns={2}>
+                  <FormField label="Tipo">
+                    <Select
+                      selectedOption={{ label: formData.tipo, value: formData.tipo }}
+                      options={[
+                        { label: 'Tesis', value: 'Tesis' },
+                        { label: 'Trabajo de investigación', value: 'Trabajo de investigación' }
+                      ]}
+                      onChange={({ detail }) => setFormData(prev => ({ ...prev, tipo: detail.selectedOption.value }))}
+                    />
+                  </FormField>
+                  <FormField label="Grado">
+                    <Select
+                      selectedOption={{ label: formData.grado, value: formData.grado }}
+                      options={[
+                        { label: 'Bachiller', value: 'Bachiller' },
+                        { label: 'Magister', value: 'Magister' },
+                        { label: 'Doctor', value: 'Doctor' },
+                        { label: 'Segunda Esp.', value: 'Segunda Esp.' },
+                        { label: 'Título Prof.', value: 'Título Prof.' }
+                      ]}
+                      onChange={({ detail }) => setFormData(prev => ({ ...prev, grado: detail.selectedOption.value }))}
+                    />
+                  </FormField>
+                </ColumnLayout>
+                <FormField label="Año">
+                  <Select
+                    selectedOption={{ label: formData.year, value: formData.year }}
+                    options={years.map(year => ({ label: year.toString(), value: year.toString() }))}
+                    onChange={({ detail }) => setFormData(prev => ({ ...prev, year: detail.selectedOption.value }))}
+                  />
+                </FormField>
+                <Container header={<Header variant="h4">Datos del Documento</Header>}>
+                  {formData.autores.map((autor, index) => (
+                    <Container
+                      key={index}
+                      header={
+                        <SpaceBetween direction="horizontal" size="m">
+                          <Header variant="h5">Datos del Autor {index + 1}</Header>
+                          {index > 0 && (
+                            <Button onClick={() => removeAuthor(index)} variant="icon" iconName="close" ariaLabel="Eliminar autor" />
+                          )}
+                        </SpaceBetween>
+                      }
+                    >
                       <SpaceBetween direction="horizontal" size="m">
-                        <Header variant="h5">Datos del Asesor {index + 1}</Header>
-                        {index > 0 && (
-                          <Button onClick={() => removeAdvisor(index)} variant="icon" iconName="close" ariaLabel="Eliminar asesor" />
-                        )}
+                        <FormField label="Nombres">
+                          <Input value={loadingDni.autores[index] ? 'Cargando...' : autor.nombre} readOnly />
+                        </FormField>
+                        <FormField label="Apellidos">
+                          <Input value={loadingDni.autores[index] ? 'Cargando...' : autor.apellido} readOnly />
+                        </FormField>
                       </SpaceBetween>
-                    }
-                  >
-                    <SpaceBetween direction="horizontal" size="m">
-                      <FormField label="Nombres">
-                        <Input value={asesor.nombre} readOnly />
-                      </FormField>
-                      <FormField label="Apellidos">
-                        <Input value={asesor.apellido} readOnly />
-                      </FormField>
-                    </SpaceBetween>
-                    <SpaceBetween direction="horizontal" size="m">
-                      <FormField label="DNI">
-                        <Input value={asesor.dni} onChange={({ detail }) => {
-                          handleChange('dni', detail.value, index, 'asesores');
-                          if (detail.value.length === 8) {
-                            fetchAndSetDataByDni(detail.value, index, 'asesores');
-                          }
-                        }} />
-                      </FormField>
-                      <FormField label="Título">
-                        <Select
-                          selectedOption={{ label: asesor.titulo, value: asesor.titulo }}
-                          options={[
-                            { label: 'Magister', value: 'Magister' },
-                            { label: 'Doctor', value: 'Doctor' }
-                          ]}
-                          onChange={({ detail }) => handleChange('titulo', detail.selectedOption.value, index, 'asesores')}
+                      <SpaceBetween direction="horizontal" size="m">
+                        <FormField label="DNI">
+                          <Input
+                            value={autor.dni}
+                            readOnly={index === 0}
+                            onChange={({ detail }) => handleDniChange(detail, index, 'autores')}
+                            type="number"
+                          />
+                        </FormField>
+                        <FormField label="Teléfono">
+                          <Input
+                            value={loadingDni.autores[index] ? 'Cargando...' : autor.telefono}
+                            readOnly={autor.telefono !== ''}
+                            onChange={({ detail }) => handleChange('telefono', detail.value, index)}
+                            type="tel"
+                          />
+                        </FormField>
+                      </SpaceBetween>
+                      <FormField label="Email UNMSM">
+                        <Input
+                          value={loadingDni.autores[index] ? 'Cargando...' : autor.email}
+                          readOnly={autor.email !== ''}
+                          onChange={({ detail }) => handleChange('email', detail.value, index)}
+                          type="email"
                         />
                       </FormField>
-                    </SpaceBetween>
-                    <FormField label="URL de ORCID">
-                      <Input value={asesor.orcid} onChange={({ detail }) => handleChange('orcid', detail.value, index, 'asesores')} />
-                    </FormField>
-                  </Container>
-                ))}
-                <Button onClick={addAdvisor} disabled={formData.asesores.length >= 2}>Agregar Asesor</Button>
+                    </Container>
+                  ))}
+                  <Button onClick={addAuthor} disabled={formData.autores.length >= 2}>Agregar Autor</Button>
+                </Container>
+                <Container header={<Header variant="h4">Datos del Asesor</Header>}>
+                  {formData.asesores.map((asesor, index) => (
+                    <Container
+                      key={index}
+                      header={
+                        <SpaceBetween direction="horizontal" size="m">
+                          <Header variant="h5">Datos del Asesor {index + 1}</Header>
+                          {index > 0 && (
+                            <Button onClick={() => removeAdvisor(index)} variant="icon" iconName="close" ariaLabel="Eliminar asesor" />
+                          )}
+                        </SpaceBetween>
+                      }
+                    >
+                      <SpaceBetween direction="horizontal" size="m">
+                        <FormField label="Nombres">
+                          <Input value={loadingDni.asesores[index] ? 'Cargando...' : asesor.nombre} readOnly />
+                        </FormField>
+                        <FormField label="Apellidos">
+                          <Input value={loadingDni.asesores[index] ? 'Cargando...' : asesor.apellido} readOnly />
+                          </FormField>
+                        </SpaceBetween>
+                      <SpaceBetween direction="horizontal" size="m">
+                        <FormField label="DNI">
+                          <Input
+                            value={asesor.dni}
+                            onChange={({ detail }) => handleDniChange(detail, index, 'asesores')}
+                            type="number"
+                          />
+                        </FormField>
+                        <FormField label="Título">
+                          <Select
+                            selectedOption={{ label: asesor.titulo, value: asesor.titulo }}
+                            options={[
+                              { label: 'Magister', value: 'Magister' },
+                              { label: 'Doctor', value: 'Doctor' }
+                            ]}
+                            onChange={({ detail }) => handleChange('titulo', detail.selectedOption.value, index, 'asesores')}
+                          />
+                        </FormField>
+                      </SpaceBetween>
+                      <FormField label="URL de ORCID">
+                        <Input
+                          value={loadingDni.asesores[index] ? 'Cargando...' : asesor.orcid}
+                          onChange={({ detail }) => handleChange('orcid', detail.value, index, 'asesores')}
+                        />
+                      </FormField>
+                    </Container>
+                  ))}
+                  <Button onClick={addAdvisor} disabled={formData.asesores.length >= 2}>Agregar Asesor</Button>
+                </Container>
               </Container>
-            </Container>
+            </div>
           </ColumnLayout>
         )}
       </SpaceBetween>
