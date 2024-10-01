39a97e7f32c63ffa47c8a2b7b1b1789721cf2c5b
CRISTINAMARTINEZZ <luzcristina.martinez@unmsm.edu.pe>
2024-10-01 11:01:23 -0500

    aa

--- src/components/dashboard/components/Notificaciones.jsx
+++ src/components/dashboard/components/Notificaciones.jsx
@@ -1,44 +1,64 @@
 import React from 'react';
-import NotificacionesHeader from './NotificacionesHeader';
-import { Container, Header, ColumnLayout, Box } from '@cloudscape-design/components';
+
+import { Container, Header, ColumnLayout, Box, Button, Badge } from '@cloudscape-design/components';
 
 const alertas = [
   {
     id: 1,
     title: 'Alerta de proceso incompleto',
-    duration: 'Le falta completar 2 tesis',
+    text: 'La cantidad de tesis que le falta completar son: ',
+    number: 2,
   },
   {
     id: 2,
     title: 'Alerta de proceso incompleto',
-    duration: 'No ha completado 2 tesis dentro de las 24 horas',
+    text: 'La cantidad de tesis que no ha completado en 24 horas son: ',
+    number: 2,
   },
   {
     id: 3,
     title: 'Alerta de proceso incompleto',
-    duration: 'Corregir 3 tesis enviado de Recepción de documentos',
+    text: 'La cantidad de tesis que necesita corregir de Recepción de documentos son: ',
+    number: 3,
   },
   {
     id: 4,
     title: 'Alerta de proceso incompleto',
-    duration: 'Falta completar',
+    text: 'La cantidad de tesis que faltan por revisar son: ',
+    number: 1,
   },
 ];
 
 const Notificaciones = () => {
   return (
     <Container>
-      <NotificacionesHeader />
+      {/* Encabezado de Advertencias */}
+      <Box 
+        padding={{ vertical: 's', horizontal: 'l' }} 
+        display="flex" 
+        alignItems="center" 
+        justifyContent="space-between"
+      >
+        <Header variant="h2" style={{ margin: 0 }}>Advertencias</Header>
+        <Badge color="blue">22</Badge>
+      </Box>
+      
+      
+      {/* Sección de Alertas */}
       <ColumnLayout columns={4} borders="vertical" variant="text-grid">
         {alertas.map((alerta) => (
-          <Container key={alerta.id} header={
-            <Header variant="h5" style={{ fontSize: '10px', display: 'flex', alignItems: 'center' }}>
-              <span style={{ fontSize: '18px', color: '#0073e6', marginRight: '5px' }}>+</span>
-              {alerta.title}
-            </Header>
-          }>
+          <Container 
+            key={alerta.id} 
+            header={
+              <Header variant="h5" style={{ fontSize: '10px', display: 'flex', alignItems: 'center', justifyContent: 'flex-start' }}>
+                <Button iconName="add-plus" variant="icon" ariaLabel="Agregar" />
+                <span style={{ marginLeft: '5px' }}>{alerta.title}</span> 
+              </Header>
+            }
+          >
             <Box style={{ fontSize: '12px' }}>
-              {alerta.duration}
+              {alerta.text}
+              <span style={{ color: 'red' }}>{alerta.number}</span>
             </Box>
           </Container>
         ))}
