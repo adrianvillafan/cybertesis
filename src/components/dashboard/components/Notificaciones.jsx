c591ad7f40b66ab105827352ee92d88eb5b5fa7b
CRISTINAMARTINEZZ <luzcristina.martinez@unmsm.edu.pe>
2024-10-01 10:11:53 -0500

    a

--- src/components/dashboard/components/Notificaciones.jsx
+++ src/components/dashboard/components/Notificaciones.jsx
@@ -4,18 +4,22 @@ import { Container, Header, ColumnLayout, Box } from '@cloudscape-design/compone
 
 const alertas = [
   {
+    id: 1, // Agrega un identificador único
     title: 'Alerta de proceso incompleto',
     duration: 'Le falta completar 2 tesis',
   },
   {
+    id: 2,
     title: 'Alerta de proceso incompleto',
     duration: 'No ha completado 2 tesis dentro de las 24 horas',
   },
   {
+    id: 3,
     title: 'Alerta de proceso incompleto',
     duration: 'Corregir 3 tesis enviado de Recepción de documentos',
   },
   {
+    id: 4,
     title: 'Alerta de proceso incompleto',
     duration: 'falta',
   },
@@ -25,10 +29,10 @@ const Notificaciones = () => {
   return (
     <Container>
       <NotificacionesHeader />
-      <ColumnLayout columns={4} borders="vertical" variant="text-grid"> {/* 4 columnas con bordes verticales */}
-        {alertas.map((alerta, index) => (
-          <Container key={index} header={<Header variant="h3">{alerta.title}</Header>}>
-            <Box>
+      <ColumnLayout columns={4} borders="vertical" variant="text-grid">
+        {alertas.map((alerta) => (
+          <Container key={alerta.id} header={<Header variant="h5" style={{ fontSize: '10px' }}>{alerta.title}</Header>}>
+            <Box style={{ fontSize: '12px' }}>
               {alerta.duration}
             </Box>
           </Container>
--- src/components/dashboard/components/NotificacionesHeader.jsx
+++ src/components/dashboard/components/NotificacionesHeader.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Header, Box, Badge } from '@cloudscape-design/components';
+import { Box, Badge } from '@cloudscape-design/components';
 
 const NotificacionesHeader = () => {
   return (
-    <Box padding={{ vertical: 's', horizontal: 'l' }} display="flex" justifyContent="space-between" alignItems="center" className="notificaciones--header">
-      <Header variant="h2">Notificaciones</Header>
-      <Badge color="blue">22</Badge>
+    <Box padding={{ vertical: 's', horizontal: 'l' }} display="flex" alignItems="center">
+      <Box display="flex" alignItems="center">
+        <h2 style={{ marginRight: '10px' }}>Advertencias</h2>
+        <Badge color="blue">22</Badge>
+      </Box>
     </Box>
   );
 };
